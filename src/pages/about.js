028a2c2572149973fec6a81f69bf7cf4358ed101
jony <jonytrees@gmail.com>
2019-12-15 20:28:20 +0200

    add page about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,9 @@
 import React from "react"
-import {Link} from "gatsby"
 import Layout from "../components/layout"
 
-export default () => {
-  <Layout>
-  <h2>
-    About Page
-  </h2>
-  </Layout>
-}
+export default () => (
+    <Layout>
+        <h1>About me</h1>
+        <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+    </Layout>
+)
